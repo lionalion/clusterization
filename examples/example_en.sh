84c4830ed373b53018f25d1e22b0ea34a912ee1a
Solovyev <non>
2017-03-31 10:28:09 +0300

    sh

--- examples/example_en.sh
+++ examples/example_en.sh
@@ -4,6 +4,10 @@ MD_DIR=../mod
 
 FILE=example_en.txt
 
+if [ ! -e $BIN_DIR ]; then
+	make
+fi
+
 $BIN_DIR/clust_w2v -fl $DATA_DIR/$FILE -w2v $MD_DIR/en.w2v  -th2 0.5 -th3 4 -th4 0.3 -out tmp.out
 perl out_claster_log.pl tmp.out $FILE $FILE.log
 rm tmp.out
