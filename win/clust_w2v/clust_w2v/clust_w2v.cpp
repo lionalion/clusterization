5abcfed9903cb27f4590a127200b868481e63dd7
Solovyev <non>
2017-04-01 15:18:55 +0300

    win

--- win/clust_w2v/clust_w2v/clust_w2v.cpp
+++ win/clust_w2v/clust_w2v/clust_w2v.cpp
@@ -23,32 +23,33 @@
  * THE SOFTWARE.
  */
 
+
+#include "stdafx.h"
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
 #include <map>
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <algorithm>
 
 #define _CRT_SECURE_NO_WARNINGS ;
 using namespace std;
 
 void error (const char * text1, const char  * text2 = ""){
-	std::cerr << text1 << ' ' << text2 << endl;
+	cerr << text1 << ' ' << text2 << endl;
 	exit(1);
 }
 void postClast (std::vector < std::vector < int > > vIdStr,std::vector < std::vector <std::map<int,float> > > vMapCls, int th3, float th4, std::vector < std::vector < int > > & vOutIdStr, std::vector < std::vector <std::map<int,float> > > & vOutMapCls){
 	std::multimap <int,int> SizeMapCls;
 	int n=0;
 	for (int k=0; k<vMapCls.size(); k++)
-		SizeMapCls.insert(std::pair<int,int>( vMapCls[k].size(),k )); 
+		SizeMapCls.insert(std::pair<int,int>( vMapCls[k].size(),k )); 	
 
 	std::vector <int> vCheck;			
-	for (std::multimap<int,int>::const_reverse_iterator rit = SizeMapCls.rbegin(); rit != SizeMapCls.rend(); ++rit){		
+	for (auto rit = SizeMapCls.rbegin(); rit != SizeMapCls.rend(); ++rit){		
 		bool flg_x=false;
 		for (int q=0; q<vCheck.size(); q++){
 			if (vCheck[q] == (*rit).second)
@@ -58,16 +59,16 @@ void postClast (std::vector < std::vector < int > > vIdStr,std::vector < std::ve
 			vOutIdStr.resize( vOutIdStr.size()+1 );
 			vOutMapCls.resize( vOutMapCls.size()+1 );
 			for (int h=0; h< vIdStr[(*rit).second].size(); h++){							
-				vOutIdStr[n].push_back(vIdStr[(*rit).second][h]); 							
+				vOutIdStr[n].push_back(vIdStr[(*rit).second][h]); 					
 				vOutMapCls[n].push_back(vMapCls[(*rit).second][h]); 
 			}
-			for ( std::multimap<int,int>::const_reverse_iterator tir = rit; tir != SizeMapCls.rend(); ++tir){
+			for ( auto tir = rit; tir != SizeMapCls.rend(); ++tir){
 				bool flg_y=false;
 				for (int q=0; q<vCheck.size(); q++){
 					if (vCheck[q] == (*tir).second)
 						flg_y=true;
 				}
-				if ((*rit).second!=(*tir).second && flg_y==false){
+				if ((*rit).second !=(*tir).second && flg_y==false){
 					int cnt=0;
 					if (vIdStr[(*tir).second].size() > th3){
 						for (int w=0; w<vIdStr[(*rit).second].size(); w++){
@@ -105,7 +106,7 @@ void firstClast (std::vector < std::map <int,float> > vCls, std::vector < int > 
 		vMapRang.insert(std::pair<int,int>(vCls[id].size(),id));
 
 	int cnt=0;
-	for ( std::multimap<int,int>::const_reverse_iterator fst = vMapRang.rbegin(); fst != vMapRang.rend(); ++fst){	
+	for ( auto fst = vMapRang.rbegin(); fst != vMapRang.rend(); ++fst){	
 		
 		bool flg=false;
 		for (int a=0; a<vIdStr.size();a++){
@@ -126,17 +127,16 @@ void firstClast (std::vector < std::map <int,float> > vCls, std::vector < int > 
 			vMapCls[cnt].push_back(vCls[ (*fst).second ]);
 			vIdStr[cnt].push_back(vId[(*fst).second ]);
 
-			for ( std::multimap<int,int>::const_reverse_iterator scnd = fst; scnd != vMapRang.rend(); ++scnd){
+			for ( auto scnd = fst; scnd != vMapRang.rend(); ++scnd){
 				if (fst!=scnd){
-					std::map<int,float>::const_iterator itv = vCls[ (*fst).second ].begin();			
+					auto itv = vCls[ (*fst).second ].begin();			
 					int comp=0;
-					for ( std::map<int,float>::const_iterator itr = vCls[ (*scnd).second ].begin(); itr != vCls[ (*scnd).second ].end(); ++itr){
-						std::map<int,float>::const_iterator itv = vCls[ (*fst).second ].begin();
+					for ( auto itr = vCls[ (*scnd).second ].begin(); itr != vCls[ (*scnd).second ].end(); ++itr){
+						auto itv = vCls[ (*fst).second ].begin();
 						itv = vCls[ (*fst).second ].find( (*itr).first );
 						if(itv != vCls[ (*fst).second ].end() )
 							comp++;
 					}
-					
 					if( vCls[ (*fst).second ].size()*th2 < comp){
 						vMapCls[cnt].push_back(vCls[ (*scnd).second ]);
 						vIdStr[cnt].push_back(vId[(*scnd).second ]);
@@ -239,14 +239,13 @@ int main(int argc, char * argv[])
 		error ("No intext file!",namefile); 	
 		exit (1);
 	}
-	ifstream wfile(w2vfile, ifstream::in);
+	ifstream wfile (w2vfile , ifstream::in );
 	if (!wfile) {
 		error ("No inw2v file!",w2vfile); 	
 		exit (1);
 	}
 
 	if (wfile.is_open()){		
-	
 		while (getline(wfile, line)) {
 			string strCls;
 			string strWrd;
@@ -278,7 +277,7 @@ int main(int argc, char * argv[])
 	std::vector < std::vector <std::map<int,float> > > vMapCls;
 	std::vector < std::map <int,float> > vCls; 
 	int idstr;
-	int cntepo=0;
+	int cntepo=0; 
 	if (myfile.is_open()){				
 		while (getline(myfile, line)) {
 			cntepo++;
@@ -298,7 +297,7 @@ int main(int argc, char * argv[])
 				else if (-1 != nPosTab  && nPosTab < nPosEnd)		{
 					strID = line.substr(nPos,nPosTab-nPos);		
 					char *buffer = new char[strID.length() + 1];
-					strcpy(buffer,  strID.c_str());
+					strcpy_s(buffer, strID.length() + 1, strID.c_str());
 					idstr = strtod(buffer,NULL);
 					nPos=nPosTab+1;
 				}
@@ -315,7 +314,7 @@ int main(int argc, char * argv[])
 					exit(1);
 				}
 				if (strWrd != "" ){
-					std::multimap<string,string>::const_iterator itMap = w2v.begin();
+					auto itMap = w2v.begin();
 					itMap = w2v.find(strWrd);
 					if(itMap != w2v.end() ){						
 						string strTmp = (*itMap).second;
@@ -330,14 +329,14 @@ int main(int argc, char * argv[])
 							if (-1 != nTab  && nTab < nEnd)		{
 								strCh = strTmp.substr(n,nTab-n);		
 								char *buffer = new char[strCh.length() + 1];
-								strcpy(buffer, strCh.c_str());
+								strcpy_s(buffer, strCh.length() + 1, strCh.c_str());
 								ncls = strtod(buffer,NULL);
 								n=nTab+1;
 							}
 							else if (-1 == nTab && n <= nEnd)	{
 								strCh = strTmp.substr(n,nEnd-n);
 								char *buffer = new char[strCh.length() + 1];
-								strcpy(buffer,  strCh.c_str());
+								strcpy_s(buffer, strCh.length() + 1, strCh.c_str());
 								wgt = strtod(buffer,NULL);
 								n=nEnd+1;
 							}
@@ -348,14 +347,13 @@ int main(int argc, char * argv[])
 							cntmap=1;
 						}
 						else{
-							std::map<int,float>::const_iterator it = cls.begin();
+							auto it = cls.begin();
 							it = cls.find(ncls);
 							if(it != cls.end() ){	
 								cls.at(ncls) = (*it).second+wgt;								
 							}
-							else{
+							else
 								cls.insert ( std::pair<int,float>(ncls,wgt) );
-							}
 						}					
 					}
 				}	
@@ -419,12 +417,10 @@ int main(int argc, char * argv[])
 	for (int c=0; c<vOutMapCls.size(); c++){
 		out <<  c << ": " ;
 		for (int x=0; x<vOutMapCls[c].size(); x++){
-			std::map<int,float> ::const_iterator it = vOutMapCls[c][x].begin(); it != vOutMapCls[c][x].end(); ++it;
+			auto it = vOutMapCls[c][x].begin(); it != vOutMapCls[c][x].end(); ++it;
 			out <<  vOutIdStr[c][x] << " " << (*it).second  << "; ";
 		}			
 		out <<  "\n" ;
 	}
 	out << endl;
 }
-
-
